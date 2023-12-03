cc7a61fddc35fab81787650caed8cd8f65828d00
Bùi Hữu Lộc <feature451@gmail.com>
2023-12-04 03:09:20 +0700

    fix: cf 403

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -19,7 +19,9 @@ class ForumController extends Controller
     public function __construct()
     {
         $this->client = new Client(['headers' => [
-            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language' => 'en-US,en;q=0.5',
         ]]);
         $this->faker = Faker::create('vi');
         $this->tr = new GoogleTranslate;
